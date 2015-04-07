1712d2c6edaca634189c325f53fae9a9710a55c2
Vladimir Adamic <vladimir.adamic@gmail.com>
2015-04-07 21:47:28 +0200

    Extract diff and patch functionality to a separate module
    - add basic tests based on example files

--- astdiff.js
+++ astdiff.js
@@ -1,57 +1,37 @@
-#!/usr/bin/env node
-
-'use strict';
-
-require('colors');
-var fs = require('fs'),
-    esprima = require('esprima'),
+var esprima = require('esprima'),
     escodegen = require('escodegen'),
     jsdiff = require('diff');
 
-var filename1 = process.argv[2];
-var filename2 = process.argv[3];
-var outputFormat = process.argv[4];
+var getDiff = function(file1, file2) {
+    var ast1 = esprima.parse(file1);
+    var ast2 = esprima.parse(file2);
 
-var ast1 = esprima.parse(fs.readFileSync(filename1));
-var ast2 = esprima.parse(fs.readFileSync(filename2));
+    var format1,
+        format2,
+        diff;
 
-var format1,
-    format2,
-    diff;
+    format1 = escodegen.generate(ast1);
+    format2 = escodegen.generate(ast2);
+    diff = jsdiff.diffLines(format2, format1);
 
-var printColoredDiffs = function(diff) {
-    diff.forEach(function(part) {
-        var color = part.added ? 'green' :
-            part.removed ? 'red' : undefined;
-        if (color) {
-            console.log(part.value[color]);
-        } else {
-            console.log(part.value);
-        }
-    });
+    var patch = jsdiff.createPatch('FILENAME HERE', format1, format2);
+
+    return patch;
 };
 
-var printRegion = function(region, prefix) {
-    region.split("\n").forEach(function(line) {
-        console.log(prefix + line);
-    });
+var applyPatch = function(file1, diff) {
+    var ast1 = esprima.parse(file1);
+
+    var format1;
+
+    format1 = escodegen.generate(ast1);
+
+    var patched = jsdiff.applyPatch(format1, diff);
+
+    return patched + '\n';
 };
 
-format1 = escodegen.generate(ast1);
-format2 = escodegen.generate(ast2);
-diff = jsdiff.diffLines(format2, format1);
-if (outputFormat === '--human') {
-    printColoredDiffs(diff);
-} else if (outputFormat === '--color') {
-    console.log('--- a/' + filename1);
-    console.log('+++ b/' + filename2);
-    diff.forEach(function(part) {
-        if (part.added) {
-            printRegion(part.value, '-');
-        } else if (part.removed) {
-            printRegion(part.value, '+');
-        }
-    });
-} else {
-    console.log(jsdiff.createPatch(filename1, format1, format2));
-}
+module.exports =  {
+        diff: getDiff,
+        patch: applyPatch
+};
---
+++ test/astdiff.js
@@ -0,0 +1,32 @@
+var should = require('chai').should(),
+    astdiff = require('../astdiff'),
+    diff = astdiff.diff,
+    patch = astdiff.patch,
+    fs = require('fs'),
+    path = require('path');
+
+exports.testDir = path.dirname(__filename);
+
+describe('#diff', function() {
+    it('returns a diff', function() {
+        var jsFile1 = fs.readFileSync(path.join(exports.testDir, 'test_files/example1.js')),
+            jsFile2 = fs.readFileSync(path.join(exports.testDir, 'test_files/example2.js')),
+            expectedDiff = fs.readFileSync(path.join(exports.testDir, 'test_files/expected1-2.diff')).toString();
+
+        var result = diff(jsFile1, jsFile2);
+
+        result.should.equal(expectedDiff);
+    });
+});
+
+describe('#patch', function() {
+    it('returns a patched file', function() {
+        var jsFile1 = fs.readFileSync(path.join(exports.testDir, 'test_files/example1.js')),
+            generatedFile = fs.readFileSync(path.join(exports.testDir, 'test_files/example2-generated.js')),
+            diff = fs.readFileSync(path.join(exports.testDir, 'test_files/expected1-2.diff')).toString();
+
+        var result = patch(jsFile1, diff);
+
+        result.should.equal(generatedFile.toString());
+    });
+});
