8e01abbebb4644c0b18c4f4673a4ae31c78c3ac7
Vladimir Adamic <vladimir.adamic@gmail.com>
2015-04-07 22:49:16 +0200

    Passing the AWESOMENESS tests -> astdiff.js source patched by itself
    - using:
         js-beautify <(ast patch astdiff.js <(ast diff astdiff.js astdiff-mod.js))

--- astdiff.js
+++ astdiff.js
@@ -1,37 +1,24 @@
 'use strict';
-
 var esprima = require('esprima'),
     escodegen = require('escodegen'),
     jsdiff = require('diff');
-
-var getDiff = function(file1, file2) {
+var getDiff = function(file1, file2, fileName) {
     var ast1 = esprima.parse(file1);
     var ast2 = esprima.parse(file2);
-
-    var format1,
-        format2,
-        diff;
-
+    var format1, format2, diff;
     format1 = escodegen.generate(ast1);
     format2 = escodegen.generate(ast2);
     diff = jsdiff.diffLines(format2, format1);
-
-    var patch = jsdiff.createPatch('FILENAME HERE', format1, format2);
-
+    var patch = jsdiff.createPatch(fileName, format1, format2);
     return patch;
 };
-
 var applyPatch = function(file1, diff) {
     var format1;
     var ast1 = esprima.parse(file1);
-
     format1 = escodegen.generate(ast1);
-
     var patched = jsdiff.applyPatch(format1, diff);
-
     return patched + '\n';
 };
-
 module.exports = {
     diff: getDiff,
     patch: applyPatch
